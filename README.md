144977da595d9ec8be235d158474f43d3967a9a2
Cynthia Hong <cynthiahong911@gmail.com>
2017-04-24 23:05:39 -0700

    added last.fm api and testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Cogs 121: Team17 
-This is the GitHub repo for Team17 for COGS 121 Sp '17. 
+# Cogs 121: Team17
+This is the GitHub repo for Team17 for COGS 121 Sp '17.
+
+https://spiritrushahri.github.io/team17/
---
+++ css/js/main.js
@@ -0,0 +1,27 @@
+/* Create a cache object
+var cache = new LastFMCache(); */
+
+/* Create a LastFM object */
+var lastfm = new LastFM({
+  apiKey    : '6df5baf8c242a7d5eef05774443864a3',
+  apiSecret : '04135ec6373e4f80b6ffa500c1cada6c'
+/*  cache     : cache */
+});
+
+/* Load some artist info. This does nothing, so far. */
+lastfm.artist.getInfo({artist: 'The xx'}, {success: function(data){
+  console.log(data);
+  /* Use data. */
+}, error: function(code, message){
+  /* Show error message. */
+}});
+
+$(document).ready(function() {
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getToptracks&user=Essychu&api_key=6df5baf8c242a7d5eef05774443864a3&limit=10&format=json&callback=?", function(json) {
+        var html = '';
+        $.each(json.toptracks.track, function(i, item) {
+            html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>";
+        });
+        $('#result').append(html);
+    });
+});
